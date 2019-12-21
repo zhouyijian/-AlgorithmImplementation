83c3b59bf3ee5331fb56c717cbe3eead2386d3f7
周一见 <mrzhouyijian@gmail.com>
2019-12-21 22:53:34 +0800

    添加快速排序

--- 高级排序/AdvancedSort/AdvancedSort/ViewController.swift
+++ 高级排序/AdvancedSort/AdvancedSort/ViewController.swift
@@ -13,66 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //var array = SortHelper.getNearSortArray(number: 3000, swapNumber: 10)
-        var array = SortHelper.getArray(number: 3000, left: -500, right: 25000)
+        var array = SortHelper.getArray(number: 100000, left: -500000, right: 250000)
         var array2 = array
-        var array3 = array
-        var array4 = array
-        SortHelper.testTime(name: "SelectionSort", array: &array, closure: selectionSort)
-        SortHelper.testTime(name: "InserSort", array: &array2, closure: inserSort)
-        SortHelper.testTime(name: "ShellSort", array: &array3, closure: shellSort)
-        SortHelper.testTime(name: "MergeSort", array: &array4, closure: mergeSort)
+        
+        SortHelper.testTime(name: "MergeSort", array: &array, closure: mergeSort)
+        SortHelper.testTime(name: "QuickSort", array: &array2, closure: quickSort)
         SortHelper.testSort(array: array)
         SortHelper.testSort(array: array2)
-        SortHelper.testSort(array: array3)
-        SortHelper.testSort(array: array4)
     }
-    
-    func selectionSort<T: Comparable>(array: inout Array<T>) {
-        for i in 0..<array.count {
-            var minIndex = i
-            for j in i..<array.count {
-                if array[j] < array[minIndex] {
-                    minIndex = j
-                }
-            }
-            array.swapAt(i, minIndex)
-        }
-    }
-    
-    func inserSort<T: Comparable>(array: inout Array<T>) {
-        if array.count > 1 {
-            for i in 1..<array.count {
-                var k = i
-                let inserNum = array[i]
-                for j in (1...i).reversed() {
-                    if inserNum < array[j - 1] {
-                        array[j] = array[j - 1]
-                        k = j - 1
-                    } else {
-                        break
-                    }
-                }
-                array[k] = inserNum
-            }
-        }
-    }
-    
-    func shellSort<T: Comparable>(array: inout Array<T>) {
-        var gap = array.count / 2 //分组个数
-        while gap > 0 {
-            var i = gap //从第 gap 个元素开始，遍历完数组，对每一组都进行插入排序
-            while i < array.count {
-                var j = i
-                //遍历每一组的所有元素，直到找到合适位置插入（要么走到这组的第一个元素，要么在中途发现自己是此组最大元素）
-                while j - gap >= 0 && array[j] < array[j - gap] {
-                    array.swapAt(j, j - gap)
-                    j -= gap
-                }
-                i += 1
-            }
-            gap /= 2
-        }
-    }
+
     
     func mergeSort<T: Comparable>(array: inout Array<T>) {
         mergeSortPrivate(array: &array, left: 0, right: array.count - 1)
@@ -148,5 +97,31 @@ class ViewController: UIViewController {
         }
     }
     
+    func quickSort<T: Comparable>(array: inout Array<T>) {
+        quickSort(array: &array, left: 0, right: array.count - 1)
+    }
+    
+    private func quickSort<T: Comparable>(array: inout Array<T>, left: Int, right: Int) {
+        if left >= right {
+            return
+        }
+        let p = partition(array: &array, left: left, right: right)
+        quickSort(array: &array, left: left, right: p)
+        quickSort(array: &array, left: p + 1, right: right)
+    }
+    
+    private func partition<T: Comparable>(array: inout Array<T>, left: Int, right: Int) -> Int {
+        var i = left + 1
+        var j = left
+        while i <= right {
+            if array[i] < array[left] {
+                array.swapAt(i, j + 1)
+                j += 1
+            }
+            i += 1
+        }
+        array.swapAt(left, j)
+        return j
+    }
 }
 
