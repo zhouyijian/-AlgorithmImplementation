adeefcf3567d1eb42bdab735c1565f6478d404c7
周一见 <mrzhouyijian@gmail.com>
2020-03-11 23:46:08 +0800

    完善最小索引堆，添加反向查找索引的数组

--- 堆排序/MaxHeap/MaxHeap/MinIndexHeap.swift
+++ 堆排序/MaxHeap/MaxHeap/MinIndexHeap.swift
@@ -12,12 +12,15 @@ struct MinIndexHeap<T: Comparable> {
     private var data: Array<T>
     private var count = 0
     private var indexs: Array<Int>
+    private var reverses: Array<Int>
     
     init(capacity: Int) {
         data = Array<T>.init()
         indexs = Array<Int>.init()
+        reverses = Array<Int>.init()
         data.reserveCapacity(capacity)
         indexs.reserveCapacity(capacity)
+        reverses.reserveCapacity(capacity)
     }
     
     init() {
@@ -27,8 +30,10 @@ struct MinIndexHeap<T: Comparable> {
     init(arr: Array<T>) {
         data = arr
         indexs = Array<Int>.init()
+        reverses = Array<Int>.init()
         for i in 0..<data.count {
             indexs.append(i)
+            reverses.append(i)
         }
         if data.count > 1 {
             for i in (0..<((data.count - 1) / 2)).reversed() {
@@ -46,8 +51,13 @@ struct MinIndexHeap<T: Comparable> {
     }
     
     mutating func insert(value: T) {
-        data.append(value)
+        if data.count <= count {
+            data.append(value)
+        } else {
+            data[count] = value
+        }
         indexs.append(count)
+        reverses.append(count)
         count += 1
         shiftUp(index: count - 1)
     }
@@ -55,6 +65,8 @@ struct MinIndexHeap<T: Comparable> {
     mutating func extractMin() -> T {
         let min = data[indexs[0]]
         indexs[0] = indexs[count - 1]
+        reverses[indexs[0]] = 0
+        reverses[indexs[count - 1]] = -1
         count -= 1
         shiftDwon(index: 0)
         return min
@@ -66,11 +78,16 @@ struct MinIndexHeap<T: Comparable> {
     }
     
     func getItem(index: Int) -> T {
-        assert(data.isEmpty)
+        assert(data.isEmpty || !contains(index: index))
         return data[index]
     }
     
+    func contains(index: Int) -> Bool {
+        return reverses[index] != -1
+    }
+    
     mutating func changeIndex(index: Int, value: T) {
+        assert(!contains(index: index))
         data[index] = value
         for (index2, i) in indexs.enumerated() {
             if i == index {
@@ -84,6 +101,8 @@ struct MinIndexHeap<T: Comparable> {
         var min = index
         while min > 0 && data[indexs[min]] < data[indexs[(min - 1) / 2]] {
             indexs.swapAt(min, (min - 1) / 2)
+            reverses[indexs[min]] = min
+            reverses[indexs[(min - 1) / 2]] = (min - 1) / 2
             min = (min - 1) / 2
         }
     }
@@ -97,6 +116,8 @@ struct MinIndexHeap<T: Comparable> {
             }
             if indexs[j] < indexs[max] {
                 indexs.swapAt(max, j)
+                reverses[indexs[max]] = max
+                reverses[indexs[j]] = j
                 max = j
             } else {
                 break
